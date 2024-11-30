897f1f1e3088c0e45fcd269451696a264d93501c
klinter007 <131816305+klinter007@users.noreply.github.com>
2024-11-30 19:00:57 +0700

    Archive nodevalue2string node

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ from .load_image_plus import LoadImagePlusKlinter
 from .aspect_selector import AspectSelector
 from .speed_ramp import SpeedRampNode
 from .string_contact_multi import StringContactMulti
-from .nodevalue2string import NodeValue2String
+# from .nodevalue2string import NodeValue2String
 
 # Mapping of node identifiers to their respective class implementations
 NODE_CLASS_MAPPINGS = {
@@ -20,7 +20,7 @@ NODE_CLASS_MAPPINGS = {
     "concat": ConcatString,
     "filter": FilterString,
     "string_contact_multi": StringContactMulti,
-    "nodevalue2string": NodeValue2String,
+    # "nodevalue2string": NodeValue2String,
     
     # Core nodes
     "YellowBus": YellowBus,
@@ -41,7 +41,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "concat": "Concat String - klinter",
     "filter": "Filter String - klinter",
     "string_contact_multi": "String Contact Multi - klinter",
-    "nodevalue2string": "Node Value to String - klinter",
+    # "nodevalue2string": "Node Value to String - klinter",
     
     # Core nodes
     "YellowBus": "Yellow Bus - klinter",
