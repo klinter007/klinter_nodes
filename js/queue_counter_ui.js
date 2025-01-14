f15c1941805de0913800aca3bd4b9a5bb999903e
klinter007 <131816305+klinter007@users.noreply.github.com>
2025-01-14 20:28:36 +0700

    Increase queue counter limit from 255 to 1000 jobs

--- js/queue_counter_ui.js
+++ js/queue_counter_ui.js
@@ -48,7 +48,7 @@ app.registerExtension({
         const iterationInput = document.createElement('input');
         iterationInput.type = 'number';
         iterationInput.min = '1';
-        iterationInput.max = '255';
+        iterationInput.max = '1000';
         iterationInput.value = '1';
         iterationInput.style.cssText = `
             width: 60px;
